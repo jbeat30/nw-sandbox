a2c838b8a7b5628d6cf0d25a95e99b9f38a81596
jbeat <jbeat0630@gmail.com>
2025-01-13 15:14:51 +0900

    feature: Add libs/ui components
    - button component
    - img card component
    - use my-react-app test

--- apps/my-react-app/src/App.tsx
+++ apps/my-react-app/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button, MentorCard} from "ui/src/components";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Button value="Click me" callback={() => console.log('Button clicked')} />
+      <MentorCard />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
